b9804d65b2730950e72e85be69771d9286fa3ba1
Stephan Hochhaus <stephan@yauh.de>
2013-09-10 16:56:41 +0200

    fix for pushing the button
    patience, young padawan - needed to add waitForSelector

--- partiestest.js
+++ partiestest.js
@@ -3,8 +3,9 @@
 
 var casper = require('casper').create({
 // some debug info is nice during coding times
-//    verbose: true,
-//    logLevel: 'debug',
+    verbose: true,
+    logLevel: 'debug',
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22',
 // Casper sets the viewport quite small initially - so let's enlarge it something an average user may see
     viewportSize: {
         width: 1280,
@@ -16,32 +17,42 @@ casper.test.comment('Hang on to your heads, testing starts!');
 casper.start('http://localhost:3000/', function() {
     this.test.assertTitle('All Tomorrow\'s Parties', 'App title is as expected');
     this.test.comment('Taking a picture to document our success');
-    this.capture('captures/step-1.png');
-});
+    this.capture('captures/01-website.png');
+
+// simple things first
+    casper.waitForSelector('#login-buttons', function() {
+        this.test.pass('login buttons found');
+    }, function() {
+        this.test.fail('login buttons missing');
+    });
 
 // wait for it - the sign-up drop down may take a little longer
-casper.waitForSelector('#login-sign-in-link', function() {
-    this.test.assertExists('#login-sign-in-link', 'Login link exists');
-    this.click('a#login-sign-in-link');
+    casper.waitForSelector('#login-sign-in-link', function() {
+        this.test.assertExists('#login-sign-in-link', 'Login link exists');
+        this.click('a#login-sign-in-link');
+    });
+
+// this creates a new acount
+    casper.then(function() {
+        this.test.assertExists('#signup-link', 'Sign up link exists'); // comment out these 2 lines
+        this.click('a#signup-link'); // to simply log in instead of signing up
+        this.test.assertExists('#login-password', 'Password field found');
+        this.test.assertExists('#login-dropdown-list', 'login dropdown list found');
+        this.fillSelectors('#login-dropdown-list', {
+            '#login-email': 'username@example.org',
+            '#login-password': 'password'
+        }, true);
+        this.capture('captures/02-logindata.png');
+        this.click('div#login-buttons-password');
+    });
+
+    casper.waitForSelector('a#login-name-link', function() {
+        this.test.pass('We are now logged in');
+        this.capture('captures/03-loggedin.png');
+    });
+
 });
 
-// now create a new account
-casper.then(function() {
-  this.test.assertExists('#signup-link', 'Sign up link exists');
-  this.click('a#signup-link');
-  this.test.assertExists('#login-password', 'Password field found');
-  this.evaluate(function (username, password) {
-         document.querySelector('#login-email').value = username;
-         document.querySelector('#login-password').value = password;
-     }, {
-         username: 'username@example.org',
-         password: 'password'
-     });
-  this.capture('captures/step-2.png');
-  this.test.comment('Now we create a new user account');
-  this.click('div#login-buttons-password');
-  this.capture('captures/step-3.png');
-});
 
 // TODO: do something with your newly created account
 
